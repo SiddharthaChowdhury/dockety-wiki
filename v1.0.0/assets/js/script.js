3a0130c78d5c92faf7821f8540105266d5a4a0b9
Siddhartha Chowdhury <siddhartha.chowdhury@akademe.in>
2016-12-05 18:35:20 +0530

    (Page): Create complete

--- v1.0.0/assets/js/script.js
+++ v1.0.0/assets/js/script.js
@@ -1,26 +1,46 @@
 $(function(){
     // Preload TREE
     // https://jsfiddle.net/Austin4Silvers/jjn9rh70/10/
-    var MenuTree = {
-        collapse: function(element) {
-            element.slideToggle(200);
-        },
-        walk: function() {
-            $('.tree-parent-anchor', '#tree').each(function() {
-                var $a = $(this);
-                var $li = $a.parent();
-                if ($a.next().is('ul')) {
-                    var $ul = $a.next();
-                    $a[0].addEventListener('click', function(e){
-                        e.preventDefault();
-                        MenuTree.collapse($ul);
-                        $a.toggleClass('active');
-                    });
-                }
-            });
-        }
-    };
-    MenuTree.walk();
+    if($('#tree').length > 0 ){
+        var MenuTree = {
+            collapse: function(element) {
+                element.slideToggle(200);
+            },
+            walk: function() {
+                $('.tree-parent-anchor', '#tree').each(function() {
+                    var $a = $(this);
+                    var $li = $a.parent();
+                    if ($a.next().is('ul')) {
+                        var $ul = $a.next();
+                        $a[0].addEventListener('click', function(e){
+                            e.preventDefault();
+                            MenuTree.collapse($ul);
+                            $a.toggleClass('active');
+                        });
+                    }
+                });
+            }
+        };
+        MenuTree.walk();
+
+        // initiating context-menu
+        var x = new _contextMenu();              
+        x.config({                               
+              contextBoxClass : 'context-box',
+              clickedOnClass : 'get_context_menu',
+              closeBtnClass : '_close',
+              // popupBesideClass : 'className',
+              // disableErrorLog: true,
+              box_position : 'bot-left',
+              displacement_px : [10,0]
+        })
+        x.run();
+
+        $('.get_context_menu').click(function(){
+            var path = $(this).attr('data-path');
+            $('.context-box').find('.create_new_btn').attr('data-path', path);
+        })
+    }
 
 	if($('#document_view').length == 1){
 
@@ -42,12 +62,6 @@ $(function(){
                     required: true
                 }
             }
-        })
-
-        
-        $('.trigger_context_menu').click(function(){
-            var path = $(this).attr('data-path');
-            $('.context-box').find('.create_new_btn').attr('data-path', path);
         })
 	}
 
